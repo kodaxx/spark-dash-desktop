a6d161c91672873efdcffc79d7029c42ccdc6bfa
Spencer Kuzara <kodaxx@gmail.com>
2018-07-20 09:33:31 -0600

    added edit menu with copy/paste/select all options

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+const Menu = require('electron').Menu
 var path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -13,9 +14,10 @@ function createWindow () {
     height: 680,
     resizable: false,
     maximizable: false,
-    webPreferences: {
-      devTools: false
-    },
+    //not able to open devTools - commenting out during beta for debug
+    //webPreferences: {
+      //devTools: false
+    //},
     icon: path.join(__dirname, 'assets/icons/png/64x64.png')
   })
 
@@ -29,7 +31,66 @@ function createWindow () {
   })
 }
 
-app.on('ready', createWindow)
+function createMenu() {
+  //Create application menu
+  const application = {
+    label: "Spark Payments",
+    submenu: [
+      {
+        label: "About Application",
+        selector: "orderFrontStandardAboutPanel:"
+      },
+      {
+        type: "separator"
+      },
+      {
+        label: "Quit",
+        accelerator: "Command+Q",
+        click: () => {
+          app.quit()
+        }
+      }
+    ]
+  }
+
+  const edit = {
+    label: "Edit",
+    submenu: [
+      {
+        label: "Cut",
+        accelerator: "CmdOrCtrl+X",
+        selector: "cut:"
+      },
+      {
+        label: "Copy",
+        accelerator: "CmdOrCtrl+C",
+        selector: "copy:"
+      },
+      {
+        label: "Paste",
+        accelerator: "CmdOrCtrl+V",
+        selector: "paste:"
+      },
+      {
+        label: "Select All",
+        accelerator: "CmdOrCtrl+A",
+        selector: "selectAll:"
+      }
+    ]
+  }
+
+  const template = [
+    application,
+    edit
+  ]
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+}
+
+app.on('ready', () => {
+  createWindow()
+  createMenu()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
